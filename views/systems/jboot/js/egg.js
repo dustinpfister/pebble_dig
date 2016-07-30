b1dcb835cc8c5c475169e4d67726cbd37f43acf4
Dustin John Pfister <dustin.pfister@gmail.com>
2016-07-29 21:29:01 -0400

    egg dig methods pulled away from public api, but can be called via westSide argument

--- views/systems/jboot/js/egg.js
+++ views/systems/jboot/js/egg.js
@@ -1,14 +1,10 @@
 // the egg object contains methods that can be used to cheat, automate the game, ect.
 var egg = (function () {
 
-
-    return {
+    var digMethods = {
 
         // (defeated in 1.7.x) dig all points in the stack
         digAll : function () {
-
-            console.log('digging all points...');
-            console.log('num of points: ' + stack.points.length);
 
             stack.points.forEach(function (point) {
 
@@ -21,31 +17,30 @@ var egg = (function () {
 
             });
 
-            // return empty string to console
-            return '';
-
         },
 
         // (defeated in 1.7.x) dig emptys that have pebble up to the max digs
-        targetedDigs : function(){
+        targetedDigs : function () {
 
-            var i = stack.points.length, digs = 0, total=0,point,
+            var i = stack.points.length,
+            digs = 0,
+            total = 0,
+            point,
             max = game.getCS().digs;
-
-            while(i--){
+            while (i--) {
 
                 point = stack.points[i];
 
                 if (typeof point.val === 'object') {
 
-                    if(point.val.amount > 0){
+                    if (point.val.amount > 0) {
 
                         // and empty comp array counts as empty space
                         point.val.comp = [];
                         total += point.val.amount;
                         digs += 1;
 
-                        if(digs === max){
+                        if (digs === max) {
 
                             break;
 
@@ -59,28 +54,30 @@ var egg = (function () {
 
             game.getCS().digs = 0;
 
-            return 'refromed basic targeted dig, found ' + total + ' pebble using ' + digs + ' digs.';
-
         },
 
         // (defeated in 1.7.x) set amount of stack point 0,0,0 to the total amount of pebble in the stack
-        supperPoint : function(){
+        supperPoint : function () {
 
             var maxPeb;
 
-            if(stack.points.length > 0){
+            if (stack.points.length > 0) {
 
                 maxPeb = game.getCS().pebbleInLand;
-                stack.getPoint(0,0,0).val.amount = maxPeb;
-                stack.getPoint(0,0,0).val.comp = [];
+                stack.getPoint(0, 0, 0).val.amount = maxPeb;
+                stack.getPoint(0, 0, 0).val.comp = [];
 
             }
 
-        },
+        }
 
+    };
+
+    // egg public api
+    return {
+
+        // submit the current stack now.
         submitNow : function () {
-
-            console.log('submitting the current stack to land_submit plugin');
 
             peb({
 
@@ -95,7 +92,6 @@ var egg = (function () {
 
             }, function (res) {
 
-                console.log('egg.submitNow: the response');
                 console.log(res.response[0]);
 
             });
@@ -106,13 +102,15 @@ var egg = (function () {
         },
 
         // west side mother#@%*er!
-        westSide : function(){
+        westSide : function (method) {
 
-            //this.digAll();  // dig all points
-            //this.targetedDigs();
-            this.supperPoint();
+            if(method === undefined){ method = 'supperPoint' }
+
+            digMethods[method]();
             this.submitNow(); // submit
             machine.changeState('title') // set app back to title state
+
+            return 'westSide! method: ' + method;
 
         }
 
